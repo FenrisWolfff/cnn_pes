2b6aeb9fc46a08a6d71d533c00d37600ae6bc790
FenrisWolfff <fenrisludwig@gmail.com>
2020-12-03 01:09:08 -0800

    h2o nn dmc

---
+++ h2o_dmc/h2o_nn_dmc.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import pyvibdmc as dmc
+from pyvibdmc.analysis import *
+from pyvibdmc.simulation_utilities import *
+from pyvibdmc import potential_manager as pm
+
+
+# In[ ]:
+
+
+if __name__ == '__main__': #if using multiprocessing on windows / mac, you need to encapsulate using this line
+    pot_dir = '/' #this directory is part of the one you copied that is outside of pyvibdmc.
+    py_file = 'h2o_nn_pot.py'
+    pot_func = 'h2o_pot' # def water_pot(cds) in h2o_potential.py
+
+    #The Potential object assumes you have already made a .so file and can successfully call it from Python
+    water_pot = pm.Potential(potential_function=pot_func,
+                          python_file=py_file,
+                          potential_directory=pot_dir,
+                          num_cores=8)
+
+    #optional num_cores parameter for multiprocessing, should not exceed the number of cores on the CPU
+    #your machine has. Can use multiprocessing.cpu_count()
+
+    # Starting Structure
+    # Equilibrium geometry of water in *atomic units*, then blown up by 1.01 to not start at the bottom of the potential.
+    water_coord = np.array([[1.81005599,  0.        ,  0.        ],
+                           [-0.45344658,  1.75233806,  0.        ],
+                           [ 0.        ,  0.        ,  0.        ]]) * 1.01
+
+    for sim_num in range(5):
+        myDMC = dmc.DMC_Sim(sim_name=f"h20_nn_{sim_num}",
+                              output_folder="h2o_nn_dmc_output",
+                              weighting='discrete', #or 'continuous'. 'continuous' keeps the ensemble size constant.
+                              num_walkers=20000, #number of geometries exploring the potential surface
+                              num_timesteps=10000, #how long the simulation will go. (num_timesteps * delta_t atomic units of time)
+                              equil_steps=500, #how long before we start collecting wave functions
+                              chkpt_every=500, #checkpoint the simulation every "chkpt_every" time steps
+                              wfn_every=1000, #collect a wave function every "wfn_every" time steps
+                              desc_wt_steps=100, #number of time steps you allow for descendant weighting per wave function
+                              atoms=['H','H','O'],
+                              delta_t=1, #the size of the time step in atomic units
+                              potential=water_pot,
+                              start_structures=np.expand_dims(water_coord,axis=0), #can provide a single geometry, or an ensemble of geometries
+                              masses=None #can put in artificial masses, otherwise it auto-pulls values from the atoms string
+        )
+        myDMC.run()
+
+
+# In[ ]:
+
+
+
+
---
+++ h2o_dmc/h2o_nn_pot.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import pyvibdmc as dmc
+from pyvibdmc.analysis import *
+from pyvibdmc.simulation_utilities import *
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+
+
+# In[3]:
+
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+
+# In[4]:
+
+
+def internals_h2o(cds):
+    analyzer = AnalyzeWfn(cds)
+    bl1 = analyzer.bond_length(0,2)
+    bl2 = analyzer.bond_length(1,2)
+    theta = analyzer.bond_angle(0,2,1)
+    return np.array((bl1,bl2,theta)).T
+
+def h2o_pot(cds):
+    model = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=(3,)),
+        Dense(64, activation=tf.nn.swish),
+        Dense(64, activation=tf.nn.swish),
+        Dense(64, activation=tf.nn.swish),
+        Dense(1, activation='linear')
+    ]
+    )
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+    model.compile(optimizer=optimizer,
+                     loss='mse',
+                     metrics=[])
+    model.load_weights('/home/fenris/McCoy_Group/conv/h2o_dmc/h2o_3x64.h5')
+    
+    internal_cds = internals_h2o(cds)
+    norm_cds = 2*(internal_cds)/3-1
+    pots_wn = (10**model.predict(norm_cds)).flatten()
+    return Constants.convert(pots_wn, 'wavenumbers', to_AU=True)
+
+# In[ ]:
+
+
+
+
